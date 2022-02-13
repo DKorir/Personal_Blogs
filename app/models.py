df2599bd7e8fece3152015c88757dd821f1cc538
DKorir <dominic.korir@student.moringaschool.com>
2022-02-13 22:12:12 +0300

    import usermixin from the flask-login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from .import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 #role
 class Role(db.Model):
     __tablename__ = 'roles'
