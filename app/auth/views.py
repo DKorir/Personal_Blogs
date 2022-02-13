8cd1b31f6a534cd278234179ab2f171e961a12d2
DKorir <dominic.korir@student.moringaschool.com>
2022-02-14 01:03:43 +0300

    create logout class

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import auth
 from flask import render_template,redirect,url_for,request,flash
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
@@ -29,3 +29,9 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
