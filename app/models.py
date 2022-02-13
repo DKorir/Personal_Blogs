b0b74f03ab0d0d832245dd96b3bbf348b4c96d7d
DKorir <dominic.korir@student.moringaschool.com>
2022-02-13 22:15:44 +0300

    import login manager from flask-login

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import LoginManager
 #role
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -12,13 +13,18 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 #user
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+
+
+
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
