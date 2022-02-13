97f3568c7f1b6773d0d7a59160164c809edd3b5e
DKorir <dominic.korir@student.moringaschool.com>
2022-02-14 00:26:59 +0300

    import validator error from wtforms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[InputRequired(),Email()])
@@ -9,3 +10,10 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
     submit = SubmitField('Sign Up')
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
