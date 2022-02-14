a82ee39dc6d50b1c6d9dbdae6e9bff0019966e8e
DKorir <dominic.korir@student.moringaschool.com>
2022-02-14 14:41:58 +0300

    install flask uploads

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 db=SQLAlchemy()
 
 bootstrap=Bootstrap()
